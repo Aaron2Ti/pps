2f07266b96937329e46d0495c5008298ca0f3d37
Aaron Tian <Aaron2Ti@gmail.com>
2009-03-09 15:19:30 +0800

    change default options for form field

--- config/environment.rb
+++ config/environment.rb
@@ -16,18 +16,13 @@ Rails::Initializer.run do |config|
   config.active_record.timestamped_migrations = false
 
   config.gem 'haml',        :version => '>= 2.0.4'
-  config.gem 'authlogic'    :version => '>= 1.4.3'
+  config.gem 'authlogic',   :version => '>= 1.4.3'
   config.gem 'mini_magick', :version => '>= 1.2.3'
   config.gem 'sqlite3-ruby', :lib     => 'sqlite3'
   # config.gem 'rmagick'#, :version => '>= 2.7.0'
-
   # config.gem 'zipruby', :version => '>= 0.2.9'
-  # config.gem 'rspec', :version => '>= 1.1.11', :lib => 'spec'
 end
 
 # ? 'application/octet-stream'
 # SolidWorks Part's MimeType is 'application/presentations' or 'image/x-presentations'
 Mime::Type.register 'application/octet-stream', :sldprt
-
-ActionView::Helpers::InstanceTag::DEFAULT_FIELD_OPTIONS = {}
-ActionView::Helpers::InstanceTag::DEFAULT_TEXT_AREA_OPTIONS = {:rows => 5}
---
+++ config/preinitializer.rb
@@ -0,0 +1,9 @@
+# overwite the defaults text_field options
+module ActionView
+  module Helpers
+    class InstanceTag
+      DEFAULT_FIELD_OPTIONS = {}
+      DEFAULT_TEXT_AREA_OPTIONS = {:rows => 5}
+    end
+  end
+end
